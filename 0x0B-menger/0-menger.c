51d2d27c3b448730aa849f3528e7d5fca6d39a09
JackoDev <untaljacko@gmail.com>
2021-03-04 20:56:27 -0500

    [fix] betty

--- 0x0B-menger/0-menger.c
+++ 0x0B-menger/0-menger.c
@@ -6,26 +6,26 @@
 
 void menger(int level)
 {
-  int n, m, j, x, y;
-  char s;
-  m = pow(3, level);
+int n, m, j, x, y;
+char s;
+m = pow(3, level);
 
-    for (n = 0; n < m; n++)
-    {
-      for (j = 0; j < m;)
-      {
-        s = '#';
-        x = n;
-        y = j++;
-        while (x > 0 || y > 0)
-        {
-          if (x % 3 == 1 && y % 3 == 1)
-            s = ' ';
-          x /= 3;
-          y /= 3;
-        }
-        printf("%c", s);
-      }
-  printf("\n");
-  }
+for (n = 0; n < m; n++)
+{
+for (j = 0; j < m;)
+{
+s = '#';
+x = n;
+y = j++;
+while (x > 0 || y > 0)
+{
+if (x % 3 == 1 && y % 3 == 1)
+s = ' ';
+x /= 3;
+y /= 3;
+}
+printf("%c", s);
+}
+printf("\n");
+}
 }
